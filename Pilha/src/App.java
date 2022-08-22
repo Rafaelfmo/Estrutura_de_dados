293ec6f43b2873821eae112da9d4348e8342d185
Rafael Oliveira <rafaelfmo11@gmail.com>
2022-08-22 17:41:14 -0300

    Estrutura Pilha

---
+++ Pilha/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Pilha pilha = new Pilha(5);
+
+        pilha.empilhar(1);
+        pilha.empilhar(2);
+        pilha.empilhar(3);
+        pilha.empilhar(4);
+        pilha.empilhar(5);
+        pilha.desempilhar();
+        pilha.desempilhar();
+        pilha.desempilhar();
+        pilha.retornarElemento(1);
+    }
+}
